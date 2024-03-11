20b0a68317e150fc352b148c9b666d2efd290840
Shadrak Lagadi <shadrl19@gmail.com>
2024-03-11 14:48:16 -0600

    Add Logic to add properties

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -186,11 +186,44 @@ if (options.minimum_rating) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = function (property, req) {
+  return new Promise((resolve, reject) => {
+
+    const sessionCookie = req.cookies.session;
+    if (!sessionCookie) {
+      reject(new Error('User is not logged in.'));
+      return;
+    }
+
+
+    const userId = extractUserIdFromSessionCookie(sessionCookie);
+
+    const queryString = `
+      INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING *;
+    `;
+    const queryParams = [
+      userId, // Use the userId extracted from the sessionCookie
+      property.title,
+      property.description,
+      property.thumbnail_photo_url,
+      property.cover_photo_url,
+      property.cost_per_night,
+      property.street,
+      property.city,
+      property.province,
+      property.post_code,
+      property.country,
+      property.parking_spaces,
+      property.number_of_bathrooms,
+      property.number_of_bedrooms
+    ];
+
+    pool.query(queryString, queryParams)
+      .then((res) => resolve(res.rows[0])) // Return the saved property
+      .catch((err) => reject(err));
+  });
 };
 
 module.exports = {
